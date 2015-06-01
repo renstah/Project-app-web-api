8881db7e613c2f063319d44c3171e2b9e9719e80
Lex <lexgoudriaan@outlook.com>
2015-06-01 21:38:15 +0200

    Database + connection

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET events listing. */
 router.get('/', function(req, res, next) {
     res.json({
         "event1": {
@@ -9,6 +9,8 @@ router.get('/', function(req, res, next) {
         }
     });
 });
+
+
 
 
 module.exports = router;
